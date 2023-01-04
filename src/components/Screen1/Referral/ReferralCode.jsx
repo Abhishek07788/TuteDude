9f47b0217af8acd5d3b8d3a332a2c67b3aece410
Abhishek Pratap Solanki <abhisheksolanki1020@gmail.com>
2023-01-04 15:25:06 +0530

    complated

--- src/components/Screen1/Referral/ReferralCode.jsx
+++ src/components/Screen1/Referral/ReferralCode.jsx
@@ -1,16 +1,13 @@
 import React from "react";
 import style from "./referral.module.css";
 
-
 const ReferralCode = () => {
   return (
-    <div>
-      {/* ------ Referral code ----------- */}
-      <div className={style.ReferralCode}>
-        <h3>Your Referral Code</h3>
-        <div>
-          <h4>EDCH54</h4>
-        </div>
+    // {/* ------ Referral code ----------- */}
+    <div className={style.ReferralCode}>
+      <h3>Your Referral Code</h3>
+      <div>
+        <h4>&emsp;EDCH54</h4>
       </div>
     </div>
   );
